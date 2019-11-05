158d828a640cdb1bceef49337788a7309b1bb362
Ibraheem Ahmed <heem4242@gmail.com>
2019-11-05 12:03:40 +0500

    added foodmatic files

---
+++ app/src/main/java/se/aroms/inventory.java
@@ -0,0 +1,14 @@
+package se.aroms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+}
