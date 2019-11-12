1ef4bababf99885560b5fe73271f20e9b32c04a7
Ali haider <l164267@lhr.nu.edu.pk>
2019-11-12 14:59:32 +0500

    upload changes-2

--- app/src/main/java/se/aroms/add_dish_ingredient.java
+++ app/src/main/java/se/aroms/add_dish_ingredient.java
@@ -1,6 +1,6 @@
 package se.aroms;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class add_dish_ingredient extends AppCompatActivity {
--- app/src/main/java/se/aroms/inventory.java
+++ app/src/main/java/se/aroms/inventory.java
@@ -3,11 +3,14 @@ package se.aroms;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+//import androidx.widget.LinearLayoutManager;
+//import androidx.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
