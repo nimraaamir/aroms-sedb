00a628732c5b06a976bfc836a538e2d6c1fa9fe3
Arshia Akhtar <Arshiaakhtar97@gmail.com>
2019-11-18 23:08:15 +0500

    Prototype added

---
+++ app/src/main/java/se/aroms/tablets.java
@@ -0,0 +1,28 @@
+package se.aroms;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class tablets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tablets);
+        //Spinner dropdown=findViewById(R.id);
+        List<String> list =new ArrayList<>();
+        list.add("Table No");
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       // dropdown.setAdapter(dataAdapter);
+
+    }
+}
---
+++ app/src/main/java/se/aroms/waiters.java
@@ -0,0 +1,14 @@
+package se.aroms;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class waiters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiters);
+    }
+}
